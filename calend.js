74384ac9148efba3a5dc1801e56e9c453fe0f887
MADHAV MANOHAR <madhav.ms@inapp.com>
2018-08-09 08:09:26 +0530

    CALEDER DATES PRINTING

--- calend.js
+++ calend.js
@@ -1,20 +1,26 @@
-
-
 var daysofweek=['sun','mon','tue','wed','thu','fri','sat'];
 var monthsofyear=['jan','feb','mar','apr','may','jun','july','aug','sep','oct','nov','dec'];
 var daysofmonth=['31','28','31','30','31','30','31','31','30','31','30','31'];
 var yearbegsun=[0,3,3,6,1,4,6,2,5,0,3,5];
 var yearbegsunleap=[0,3,4,0,2,5,0,3,6,1,5,6];
-var a = new Array();
+var a = [];
+    for(var i = 0; i <12; ++i) { a.push(''); }
+    console.log('this is a');
+    console.log(a);
 cal();
 function cal()
 {
-  var month=process.argv.slice(2);
+  var month=process.argv[2];
   var year=process.argv.slice(3);
   console.log('is leap or not');
   console.log(isleap(year));
   validate(year,month);
+  console.log('month is');
+  console.log(month);
+
   setdatearray(year,month);
+  console.log('array after set');
+  console.log(yearbegsun);
   getcalender(year,month);
 
   printcalender(year,month);
@@ -39,10 +45,97 @@ if (month>12)
 
 }
 
+
+function getcalender(year,month)
+{
+
+console.log("hero");
+var x=0
+var z=1
+var k
+if(isleap(year)==true)
+k=yearbegsunleap[month-1]
+else
+k=yearbegsun[month-1]
+  console.log('k values')
+
+  console.log(yearbegsunleap[month-1])
+  console.log(month)
+if(k>0)
+{
+  for(i=0;i<k;i++)
+    a[x]=a[x]+' '+' '
+
+
+  for(i=k;i<7;i++)
+    {a[x]=a[x]+' '+z
+    z++
+    }
+
+
+
+
+  while(z<daysofmonth[month-1])
+  {
+  for(i=1;i<6;i++)
+  {x++
+    for(i=0;i<7;i++)
+    {
+      a[x]=a[x]+' '+z
+      z++
+
+    }
+    }
+  }
+}
+
+
+else {
+  while(z<daysofmonth[month-1])
+  {
+  for(i=0;i<6;i++)
+  {while(z<daysofmonth[month-1])
+    for(i=0;i<7;i++)
+    {
+      a[x]=a[x]+' '+z
+      z++
+    }
+    x++
+  }
+}
+}
+}
+
+
+
+function printcalender(year,month)
+{
+console.log('hello')
+
+for(var i=0;i<6;i++)
+
+console.log(a[i])
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function isleap(year)
 
 {
-if((year % 4 == 0) && ((year % 100 != 0) || (year % 400 == 0)))
+if((year % 4 == 0) && (year % 100 != 0) || (year % 400 == 0))
 return true;
 else
 return false;
@@ -67,102 +160,53 @@ function startdayofweek(year,month)
   console.log('year');
   console.log(year);
   var startday=((year-b)*365+(b*366))%7;
+  startday-=1;
   console.log('startday');
   console.log(startday);
-  return startday-1
+  return startday
   }
 
 
 function setdatearray(year,month)
-{console.log("set date");
+{console.log('array before set');
+console.log(yearbegsun);
+  console.log("set date");
 c=startdayofweek(year,month);
 console.log(c);
 diff=c-yearbegsun[0];
+console.log('diff');
+console.log(diff);
 diffarr=[diff,diff,diff,diff,diff,diff,diff,diff,diff,diff,diff,diff];
-if(isleap==false)
+console.log('diffarr');
+console.log(diffarr);
+if(isleap(year)==false)
 {
-yearbegsun=yearbegsun+diffarr
-for(i=0;i<12;i++)
+  console.log('madhav this is not leap');
+  for(var i = 0; i < 12; i++){
+     yearbegsun[i]=yearbegsun[i] + diffarr[i];
+     console.log('HELLOOOO');
+     console.log(yearbegsun[i]);
+   }
+
+     for(i=0;i<12;i++)
 {
-  yearbegsun[i]=yearbegsun[i]%7
+       yearbegsun[i]=yearbegsun[i]%7
+       console.log('hello madhav this is not leap');
+       console.log(yearbegsun[i]);
+  }
 }
-}
+
+
 else{
-  yearbegsunleap=yearbegsunleap+diffarr
-  for(i=0;i<12;i++){
-    yearbegsunleap[i]=yearbegsunleap[i]%7
-  }
-
-}
-}
-
-
-
-
-function noofrow(year,month)
-{
-  var daysInWeek = 7
-  if(isleap==true){
-  var startDay =  yearbegsun[month-1]}
-  else{
-  var startDay =  yearbegsunleap[month-1]}
-
-  var daysInMonth = daysofmonth[month-1]
-  var numRows = (startDay+daysInMonth)/daysInWeek
-
-  if((startDay+daysInMonth - 1) % daysInWeek != 0)
-      numRows +=1
-
-
-  return numRows
-}
-
-
-
-
-function getcalender(year,month)
-{console.log("hero");
-var x=0
-var z=1
-if(isleap==true)
-var k=yearbegsunleap[month-1]
-else
-  var k=yearbegsun[month-1]
-if(k>0)
-{
-  for(i=0;i<k;i++){noofrow(year,month)
-    a[x]=a[x]+' '
-
-
-  for(i=k;i<7;i++){
-    a[x]=a[x]+' '+z
-    z++
-  }
-  x++
-  while(z<32)
-  {
-  for(i=1;i<numRow;i++)
-  {
-    for(i=1;i<7;i++)
-    {
-      a[x]=a[x]+' '+z
-      z++
-    }
-    x++
+  console.log('madhav this is leap');
+  for(var i = 0; i < 12; i++)
+  {yearbegsunleap[i]=(yearbegsunleap[i] + diffarr[i]);
+   console.log('madhav this is leap');
+   console.log(yearbegsunleap[i]);}
+  for(var i=0;i<12;i++){
+    yearbegsunleap[i]=yearbegsunleap[i]%7;
+    console.log('hello madhav this is leap');
+    console.log(yearbegsunleap[i]);
   }
 }
-}
-}
-}
-
-
-function printcalender(year,month)
-{
-console.log('hello')
-x=noofrow(year,month);
-console.log(x)
-for(i=0;i<10;i++)
-console.log('hello')
-console.log(a[i])
-
 }
